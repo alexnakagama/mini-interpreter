7d700da908b8886c25f5437b8e4acd01e6329663
alexnakagama <alexnakagama4@gmail.com>
2026-06-16 13:19:15 -0300

    added comments explaning how the vec_push works

--- src/vector/vector.c
+++ src/vector/vector.c
@@ -14,6 +14,15 @@ Vector *vec_init(Vector *vec) {
     return vec;
 }
 
+/*
+    vec_push receives a pointer to the vector and a value we want to insert
+    if the vector is full (vec->len == vec-cap) we need more memory, we create a temporal var
+    when the vector is recently created we put an initial capacity of 8
+    in the other case we duplicate the capacity of the vector new_cap = vec->cap * 2
+    with realloc we ask more memory
+    we update the pointer and the capacity vec->items = temp / vec->cap = new_cap
+    then insert the elements with vec->items[vec-len] and increase the len with ++
+*/
 void vec_push(Vector *vec, int value) {
     if (!vec) {
         return;
