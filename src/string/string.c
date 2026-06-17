77a2f6fe0d70e8ab057d373d0b98159b00869459
alexnakagama <alexnakagama4@gmail.com>
2026-06-17 16:43:33 -0300

    implementation of string

--- src/string/string.c
+++ src/string/string.c
@@ -2,6 +2,7 @@
 
 #include <stddef.h>
 #include <stdlib.h>
+#include <string.h>
 
 String *str_init(String *str) {
     if (!str) {
@@ -16,7 +17,37 @@ String *str_init(String *str) {
 }
 
 void str_append(String *str, const char *text) {
+    if (!str || !text) {
+        return;
+    }
 
+    size_t text_len = strlen(text);
+
+    while (str->len + text_len + 1 > str->cap) {
+        size_t new_cap = str->cap;
+        if (str->cap == 0) {
+            new_cap = 8;
+        } else {
+            new_cap = str->cap * 2;
+        }
+
+        char *temp = realloc(str->data, new_cap);
+        if (!temp) {
+            return;
+        }
+
+        str->data = temp;
+        str->cap = new_cap;
+    }
+
+    memcpy(
+        str->data + str->len,
+        text,
+        text_len
+    );
+
+    str->len += text_len;
+    str->data[str->len] = '\0';
 }
 
 void str_free(String *str) {
