88b1bcab672a2f78bf71dc769588ef5563ee7d8a
alexnakagama <alexnakagama4@gmail.com>
2026-06-23 15:56:08 -0300

    organized code better

--- src/string/string.c
+++ src/string/string.c
@@ -5,8 +5,7 @@
 #include <string.h>
 
 void str_init(String *str) {
-    if (!str)
-        return;
+    if (!str) return;
 
     str->data = NULL;
     str->len = 0;
@@ -14,22 +13,18 @@ void str_init(String *str) {
 }
 
 void str_append(String *str, const char *text) {
-    if (!str || !text)
-        return;
+    if (!str || !text) return;
 
     size_t text_len = strlen(text);
 
     while (str->len + text_len + 1 > str->cap) {
         size_t new_cap = str->cap;
 
-        if (str->cap == 0)
-            new_cap = 8;
-        else
-            new_cap = str->cap * 2;
+        if (str->cap == 0) new_cap = 8;
+        else new_cap = str->cap * 2;
 
         char *temp = realloc(str->data, new_cap * sizeof(char));
-        if (!temp)
-            return;
+        if (!temp) return;
 
         str->data = temp;
         str->cap = new_cap;
@@ -42,41 +37,34 @@ void str_append(String *str, const char *text) {
 }
 
 void str_trim_left(String *str) {
-    if (!str)
-        return;
+    if (!str) return;
 
-    if (str->len == 0)
-        return;
+    if (str->len == 0) return;
 
     for (size_t i = 0; i < str->len; i++) {
     }
 }
 
 void str_trim_right(String *str) {
-    if (!str)
-        return;
+    if (!str) return;
 
-    if (str->len == 0)
-        return;
+    if (str->len == 0) return;
 
     for (size_t i = str->len - 1; i < str->len; i++) {
     }
 }
 
 void str_trim(String *str) {
-    if (!str)
-        return;
+    if (!str) return;
 
-    if (str->len == 0)
-        return;
+    if (str->len == 0) return;
 
     str_trim_left(str);
     str_trim_right(str);
 }
 
 void str_free(String *str) {
-    if (!str) 
-        return;
+    if (!str) return;
 
     free(str->data);
     str->data = NULL;
@@ -90,10 +78,8 @@ size_t str_len(const String *str) {
 }
 
 void str_clear(String *str) {
-    if (!str)
-        return;
+    if (!str) return;
 
     str->len = 0;
-    if (str->data)
-        str->data[0] = '\0';
+    if (str->data) str->data[0] = '\0';
 }
